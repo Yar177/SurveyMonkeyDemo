7516c8483145433729e0587ac4ac1758988a9088
Yar177 <yarsher.dev@gmail.com>
2019-12-03 14:11:47 -0500

    button

--- app/src/main/java/com/yar/surveymonkeydemo/MainActivity.java
+++ app/src/main/java/com/yar/surveymonkeydemo/MainActivity.java
@@ -1,14 +1,29 @@
 package com.yar.surveymonkeydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.surveymonkey.surveymonkeyandroidsdk.SurveyMonkey;
+import com.surveymonkey.surveymonkeyandroidsdk.utils.SMError;
+
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private SurveyMonkey sm = new SurveyMonkey();
+    public static final String SAMPLE_APP = "Sample App";
+    public static final String SURVEY_HASH = "LBQK27G";
+    public static final int SM_REQUEST_CODE = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sm.onStart(this, SAMPLE_APP, SM_REQUEST_CODE, SURVEY_HASH);
+
     }
+
+    public void takeSurvey(){
+
+    }
+
 }
